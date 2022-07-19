77fdddbd0ac25105f7aa343782230f755db6226e
Justin Lam <justin.kh.lam@gmail.com>
2022-07-19 01:18:22 -0400

    add in initial base style. remove app.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import styled from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
 
 const Main = styled.div``;
 const Wrapper = styled.div``;
