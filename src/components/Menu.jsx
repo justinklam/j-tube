68329f1884e6964707f9ed5345871714cd753ed2
Justin Lam <justin.kh.lam@gmail.com>
2022-07-19 01:18:32 -0400

    add in styled components into Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+`;
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return <Container>Menu</Container>;
 };
 
 export default Menu;
