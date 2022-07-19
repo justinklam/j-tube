b78f0c9c72450df7bee1da6fa918affd7b35fd23
Justin Lam <justin.kh.lam@gmail.com>
2022-07-19 01:05:33 -0400

    import in menu component. add in Main styled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import styled from "styled-components";
 
+// Components
+import Menu from "./components/Menu";
+
 const Container = styled.div``;
 
+const Main = styled.div``;
+
 function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Menu />
+      <Main></Main>
+    </Container>
+  );
 }
 
 export default App;
