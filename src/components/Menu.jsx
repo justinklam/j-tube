4f92a4c4ddfb9799d4ec6c9f34295860790acbf7
Justin Lam <justin.kh.lam@gmail.com>
2022-07-21 12:58:25 -0400

    add in Login element with button. add in base styling for button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -46,16 +46,29 @@ const Img = styled.img`
   height: 25px;
 `;
 
-const Hr = styled.hr`
-margin: 15px; 0px;
-border: 0.5px solid #373737`;
-
 const Item = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
   cursor: pointer;
   padding: 7.5px 0px;
+`;
+
+const Hr = styled.hr`
+margin: 15px; 0px;
+border: 0.5px solid #373737`;
+
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
 `;
 
 const Menu = () => {
@@ -89,6 +102,12 @@ const Menu = () => {
           <HistoryOutlinedIcon />
           History
         </Item>
+        <Hr />
+
+        <Login>
+          Sign in to like videos, comment, and subscribe
+          <Button>SIGN IN</Button>
+        </Login>
         <Hr />
 
         <Item>
