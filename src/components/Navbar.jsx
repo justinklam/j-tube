7cfc68e3e9004839116cd0053917e3e55a9499ec
Justin Lam <justin.kh.lam@gmail.com>
2022-07-23 12:01:03 -0400

    initialize styled component elements for NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+
+const Search = styled.div``;
+
+const Input = styled.input``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
 
 const Navbar = () => {
   return <div>Navbar</div>;
