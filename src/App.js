273ddfc6ed02f82e93f86b7993a8c79f2ae6061d
Justin Lam <justin.kh.lam@gmail.com>
2022-07-23 11:55:25 -0400

    setDarkMode switch for Main container

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const Container = styled.div`
 
 const Main = styled.div`
   flex: 7;
-  background-color: #181818;
+  background-color: ${({ theme }) => theme.bg};
 `;
 const Wrapper = styled.div``;
 
