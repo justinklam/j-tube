b744c42454b3f239cd88fb6bea5e96619576389e
Justin Lam <justin.kh.lam@gmail.com>
2022-07-25 11:59:53 -0400

    import Card component into Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+// Components
+import Card from "../components/Card";
+
 const Container = styled.div``;
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <Card />
+    </Container>
+  );
 };
 
 export default Home;
