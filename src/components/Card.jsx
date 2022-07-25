595a07578d40a175db7d8ea93eb053904d1b4556
Justin Lam <justin.kh.lam@gmail.com>
2022-07-25 11:58:22 -0400

    correct styled components for Container

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled`
-`;
+const Container = styled.div``;
 
 const Card = () => {
-  return <Container>Home</Container>;
+  return <Container>Card</Container>;
 };
 
 export default Card;
