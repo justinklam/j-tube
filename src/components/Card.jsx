aa935d8e55c527f133410461f221104957725e5b
Justin Lam <justin.kh.lam@gmail.com>
2022-07-25 12:25:53 -0400

    add in Details/Channel Image element for Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,10 +13,26 @@ const Image = styled.img`
   background-color: #999;
 `;
 
+const Details = styled.div`
+  display: flex;
+  margin-top: 16px;
+  gap: 12px;
+`;
+
+const ChannelImage = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+`;
+
 const Card = () => {
   return (
     <Container>
       <Image src="https://i.ytimg.com/vi/IjMESxJdWkg/hq720_live.jpg?sqp=CKz--pYG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAyQzog-iBzUmBVIP5l7wqAqErk4A" />
+      <Details>
+        <ChannelImage />
+      </Details>
     </Container>
   );
 };
