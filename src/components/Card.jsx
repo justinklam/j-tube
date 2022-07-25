467b93ea85f1f7d86906422df917e2cc65115b58
Justin Lam <justin.kh.lam@gmail.com>
2022-07-25 11:56:12 -0400

    initialize Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled`
+`;
+
+const Card = () => {
+  return <Container>Home</Container>;
+};
+
+export default Card;
