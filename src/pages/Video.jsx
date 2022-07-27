01178384c449c86db6124a2b026c8c9601744701
Justin Lam <justin.kh.lam@gmail.com>
2022-07-27 11:40:33 -0400

    initialize Video elements

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div``;
+
+const Recommendation = styled.div``;
 
 const Video = () => {
   return <div>Video</div>;
