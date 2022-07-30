27a626738326580a016bd2c38e270985dc8181e9
Justin Lam <justin.kh.lam@gmail.com>
2022-07-30 00:40:35 -0400

    add in additional elements into Comment. add base styling

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 30px 0px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Comment = () => {
-  return <Container>Comment</Container>;
+  return (
+    <Container>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    </Container>
+  );
 };
 
 export default Comment;
