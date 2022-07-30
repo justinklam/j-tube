f9f3309bf12d4591143759fbc110c7ea244afe5a
Justin Lam <justin.kh.lam@gmail.com>
2022-07-30 00:30:34 -0400

    add in additional styled elements. add in placeholder image

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,8 +3,21 @@ import styled from "styled-components";
 
 const Container = styled.div``;
 
+const NewComment = styled.div``;
+
+const Avatar = styled.img``;
+
+const Input = styled.input``;
+
 const Comments = () => {
-  return <Container>Comments</Container>;
+  return (
+    <Container>
+      <NewComment>
+        <Avatar src="https://images.pexels.com/photos/2071881/pexels-photo-2071881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <Input placeholder="Add a comment..."></Input>
+      </NewComment>
+    </Container>
+  );
 };
 
 export default Comments;
