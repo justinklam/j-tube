49ac3b19ad8564bfa5b27814434f6e216607de40
Justin Lam <justin.kh.lam@gmail.com>
2022-08-01 00:27:18 -0400

    add flex size for Image and Details elements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,12 +15,14 @@ const Image = styled.img`
   height: ${(props) => (props.type === "sm" ? "120px" : "200px")};
   background-color: #999;
   gap: 10px;
+  flex: 1;
 `;
 
 const Details = styled.div`
   display: flex;
   margin-top: ${(props) => props.type !== "sm" && "16px"};
   gap: 12px;
+  flex: 1;
 `;
 
 const ChannelImage = styled.img`
@@ -32,15 +34,18 @@ const ChannelImage = styled.img`
 `;
 
 const Texts = styled.div``;
+
 const Title = styled.h1`
 font-size: 16px;
 font-weight: 500;
 color: #{({theme}) => theme.text}`;
+
 const ChannelName = styled.h2`
   font-size: 14px;
   color: ${({ theme }) => theme.textSoft};
   margin: 9px 0px;
 `;
+
 const Info = styled.div`
 font-size: 14px
 color: ${({ theme }) => theme.textSoft};
