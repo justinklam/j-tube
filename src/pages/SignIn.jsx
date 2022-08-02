004e6d6b325727b8d6499cf2c45188dbb8a04568
Justin Lam <justin.kh.lam@gmail.com>
2022-08-02 11:12:06 -0400

    add in styling for base container

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 56px);
+`;
 
 const SignIn = () => {
   return <Container>SignIn</Container>;
