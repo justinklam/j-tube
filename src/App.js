7399f7bb31234c553e9ea383f4a917f887e67fee
Justin Lam <justin.kh.lam@gmail.com>
2022-08-02 11:02:27 -0400

    syntax cleanpu

--- src/App.js
+++ src/App.js
@@ -35,9 +35,10 @@ function App() {
             <Wrapper>
               <Routes>
                 <Route path="/">
-                  <Route index element={<Home />}></Route>
+                  <Route index element={<Home />} />
+                  <Route index element={<Home />} />
                   <Route path="video">
-                    <Route path=":id" element={<Video />}></Route>
+                    <Route path=":id" element={<Video />} />
                   </Route>
                 </Route>
               </Routes>
