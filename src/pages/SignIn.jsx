e4507e24ab4e9f8c6ea10c39a28ed17216636f45
Justin Lam <justin.kh.lam@gmail.com>
2022-08-02 11:18:02 -0400

    add in additional elements. set up input fields

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -14,13 +14,30 @@ const Wrapper = styled.div`
   align-items: center;
   flex-direction: column;
   background-color: ${({ theme }) => theme.bgLighter};
+  border: 1px solid: ${({ theme }) => theme.bgSoft};
   padding: 20px 50px;
+  gap: 10px;
 `;
+
+const Title = styled.h1``;
+
+const SubTitle = styled.h2``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
+
+const More = styled.div``;
 
 const SignIn = () => {
   return (
     <Container>
-      <Wrapper>SignIn</Wrapper>
+      <Wrapper>
+        <Title>Sign In</Title>
+        <SubTitle>to continue to J-Tube</SubTitle>
+        <Input placeholder="username" />
+        <Input type="password" placeholder="password" />
+      </Wrapper>
     </Container>
   );
 };
