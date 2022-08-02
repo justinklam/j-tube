593a03883807fa197a90c29a493de02106414592
Justin Lam <justin.kh.lam@gmail.com>
2022-08-02 11:05:26 -0400

    add styled component into SignIn component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return <Container>SignIn</Container>;
 };
 
 export default SignIn;
