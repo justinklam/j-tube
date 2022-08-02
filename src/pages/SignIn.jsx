9fdcc3756fa5302ab083870a9d57990a4d45db70
Justin Lam <justin.kh.lam@gmail.com>
2022-08-02 11:14:15 -0400

    add in Wrapper element

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,10 +6,23 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   height: calc(100vh - 56px);
+  color: ${({ theme }) => theme.text};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.bgLighter};
+  padding: 20px 50px;
 `;
 
 const SignIn = () => {
-  return <Container>SignIn</Container>;
+  return (
+    <Container>
+      <Wrapper>SignIn</Wrapper>
+    </Container>
+  );
 };
 
 export default SignIn;
