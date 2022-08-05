cf5c86560bef7f39e9d0498b3dff0fbfd6c6cb6d
Justin Lam <justin.kh.lam@gmail.com>
2022-08-05 00:15:17 -0400

    install dotenv. import dotenv and mongoose into index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.listen(8080, () => {
   console.log("Server connected! 🐅");
