dad91db8caecdbfde4eaf99c70961d53f4f05d42
Justin Lam <justin.kh.lam@gmail.com>
2022-08-05 00:42:42 -0400

    initialize Video model

---
+++ server/models/Video.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Video", VideoSchema);
