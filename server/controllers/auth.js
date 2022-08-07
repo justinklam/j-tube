a8b817441630acd96955b4ce15e1521e04b65eca
Justin Lam <justin.kh.lam@gmail.com>
2022-08-07 01:08:18 -0400

    add in error handler in auth.js. add in signin to routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+// Helper function
+import { createError } from "../error.js";
+
 // Model
 import User from "../models/User";
 
@@ -12,6 +15,17 @@ export const signup = async (req, res, next) => {
 
     await newUser.save();
     res.status(200).send("User has been created!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      name: req.body.name,
+    });
+    if (!user) return next(createError(404, "User not found!"));
   } catch (err) {
     next(err);
   }
