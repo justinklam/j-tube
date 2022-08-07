8099a82082a652ee60e048deb53672f2b887a68f
Justin Lam <justin.kh.lam@gmail.com>
2022-08-07 00:46:22 -0400

    add in bcryptjs

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 // Model
 import User from "../models/User";
 
 export const signup = async (req, res) => {
-  console.log(req.body);
-  // try {
-  //   const newUser = new User(req.body);
-  // } catch (err) {}
+  // console.log(req.body);
+  try {
+    const newUser = new User({ ...req.body });
+  } catch (err) {}
 };
