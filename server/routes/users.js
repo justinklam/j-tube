4e1eb538b2325e4c13cf66c3fd8efd3859790d84
Justin Lam <justin.kh.lam@gmail.com>
2022-08-08 01:12:01 -0400

    initialize routes needed for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,21 @@
 import express from "express";
-import {} from "../controllers/user.js";
+import { update } from "../controllers/user.js";
 
 const router = express.Router();
+
+// update user
+router.put("/:id", update);
+
+// delete user
+
+// get a user
+
+// subscribe a user
+
+// unsubscribe a user
+
+// like a video
+
+// dislike a video
 
 export default router;
