b62c12de5309abcc403f5b4481f1fc37c63c4e04
Justin Lam <justin.kh.lam@gmail.com>
2022-08-10 00:45:21 -0400

    add in logic for getUser request

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -38,12 +38,39 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-export const getUser = async (req, res, next) => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const subscribe = async (req, res, next) => {};
+export const subscribe = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const unsubscribe = async (req, res, next) => {};
+export const unsubscribe = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const like = async (req, res, next) => {};
+export const like = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const dislike = async (req, res, next) => {};
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
