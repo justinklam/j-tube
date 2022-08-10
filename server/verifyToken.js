c792be4baa87ba1894b9c3b77be6343e168bf6eb
Justin Lam <justin.kh.lam@gmail.com>
2022-08-09 21:09:11 -0400

    add in logic to send error message on invalid authentication

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
+import { createError } from "./error";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
+  if (!token) return next(createError(401, "Authentication Failed!"));
 };
