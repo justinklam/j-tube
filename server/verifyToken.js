fd5d9aaabd8c92fcbc76273d9cb3e9e94cfe7c78
Justin Lam <justin.kh.lam@gmail.com>
2022-08-09 21:11:19 -0400

    add in jwt verification to check of token matches user info

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -4,4 +4,9 @@ import { createError } from "./error";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(createError(401, "Authentication Failed!"));
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+    req.user = user;
+  });
 };
