ed5359a1cee146368317eccbf24fbd0c5261d726
Justin Lam <justin.kh.lam@gmail.com>
2022-08-09 21:13:06 -0400

    add comments in verifyToken for clarity. add verifyToken into userRoutes

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -7,6 +7,9 @@ export const verifyToken = (req, res, next) => {
 
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, "Token is not valid!"));
+    // assign jwt object user to req.user
     req.user = user;
+    // if verified, the route continues
+    next();
   });
 };
