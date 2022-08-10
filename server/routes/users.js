966af19f844c3f62af147ac777e9a23a68b2f470
Justin Lam <justin.kh.lam@gmail.com>
2022-08-09 20:50:00 -0400

    define base routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { update } from "../controllers/user.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  update,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
@@ -7,15 +15,21 @@ const router = express.Router();
 router.put("/:id", update);
 
 // delete user
+router.delete("/:id", deleteUser);
 
 // get a user
+router.get("/find/:id", getUser);
 
 // subscribe a user
+router.put("/sub/:id", subscribe);
 
 // unsubscribe a user
+router.put("/unsub/:id", unsubscribe);
 
 // like a video
+router.put("/like/:videoId", like);
 
 // dislike a video
+router.put("/dislike/:videoId", dislike);
 
 export default router;
