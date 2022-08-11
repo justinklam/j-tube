653e2146f952b89f03c337e14abf749776ec2b36
Justin Lam <justin.kh.lam@gmail.com>
2022-08-11 00:53:51 -0400

    add logic to addVideo

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -9,6 +9,8 @@ export const addVideo = async (req, res, next) => {
   const newVideo = new Video({ userId: req.user.id, ...req.body });
 
   try {
+    const savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
   } catch (err) {
     next(err);
   }
