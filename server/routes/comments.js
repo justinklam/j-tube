ac0558fb339e15c6248caded79a639e92b85f53c
Justin Lam <justin.kh.lam@gmail.com>
2022-08-14 00:54:57 -0400

    initialize first route. import in controller function and helper function for base route

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,6 +1,11 @@
 import express from "express";
-import {} from "../controllers/comment.js";
+import { addComment } from "../controllers/comment.js";
+
+// Helper function
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
 
 export default router;
