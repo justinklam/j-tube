411761f95e9ca2814f941dbc6f512bdeff14ad75
Justin Lam <justin.kh.lam@gmail.com>
2022-08-14 00:58:36 -0400

    add in logic for addComment

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,5 +1,8 @@
 export const addComment = async (req, res, next) => {
+  const newComment = new Comment({ ...req.body, userId: req.user.id });
   try {
+    const savedComment = await newComment.save();
+    res.status(200).send(savedComment);
   } catch (err) {
     next(err);
   }
