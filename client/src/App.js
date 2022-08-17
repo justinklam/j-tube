e670e8a77d943dca4e039b7504fb5f8ca79af51a
Justin Lam <justin.kh.lam@gmail.com>
2022-08-17 00:25:05 -0400

    add additional routes for trends/subs. add in props

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,9 @@ function App() {
             <Wrapper>
               <Routes>
                 <Route path="/">
-                  <Route index element={<Home />} />
+                  <Route index element={<Home type="random" />} />
+                  <Route path="trends" index element={<Home type="trend" />} />
+                  <Route path="sub" index element={<Home type="sub" />} />
                   <Route path="signin" index element={<SignIn />} />
                   <Route path="video">
                     <Route path=":id" element={<Video />} />
