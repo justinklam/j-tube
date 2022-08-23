eeeafb6a8cdb75e967148035e6af00d1ac37050c
Justin Lam <justin.kh.lam@gmail.com>
2022-08-23 00:25:42 -0400

    import redux-persist into store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import videoReducer from "./videoSlice";
+
+// Persist
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
 export default configureStore({
   reducer: {
