11b6550759629a872def16e765a080d4389ba027
Justin Lam <justin.kh.lam@gmail.com>
2022-08-24 01:01:06 -0400

    import getAuth/GoogleAuthProvider from firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_KEY,
@@ -11,3 +12,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+
+export default app;
