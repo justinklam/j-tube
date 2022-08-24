a00d8393c7de6b8c4c3d6a339556d2e2c077439e
Justin Lam <justin.kh.lam@gmail.com>
2022-08-24 00:43:23 -0400

    initialize firebase.js for firebase config

---
+++ client/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "j-tube-d1672.firebaseapp.com",
+  projectId: "j-tube-d1672",
+  storageBucket: "j-tube-d1672.appspot.com",
+  messagingSenderId: "437545735648",
+  appId: "1:437545735648:web:2676a645d91d075260c785",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
