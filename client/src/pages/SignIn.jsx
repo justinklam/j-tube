4f7e1a02f28bb6a6552a66eed1ec43eca25f12f8
Justin Lam <justin.kh.lam@gmail.com>
2022-08-25 14:09:30 -0400

    add in dispatch, successful route logic and error handler

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -93,16 +93,22 @@ const SignIn = () => {
   };
 
   const signInWithGoogle = async () => {
+    dispatch(loginStart());
+
     signInWithPopup(auth, provider)
       .then((result) => {
-        axios.post("/auth/google", {
-          name: result.user.displayName,
-          email: result.user.email,
-          img: result.user.photoURL,
-        });
+        axios
+          .post("/auth/google", {
+            name: result.user.displayName,
+            email: result.user.email,
+            img: result.user.photoURL,
+          })
+          .then((res) => {
+            dispatch(loginSuccess(res.data));
+          });
       })
       .catch((error) => {
-        console.log(error);
+        dispatch(loginFailure());
       });
   };
 
