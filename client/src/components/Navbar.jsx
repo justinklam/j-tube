c69a242706714e26918b8b0820906a26aeabd4e2
Justin Lam <justin.kh.lam@gmail.com>
2022-08-27 00:42:09 -0400

    set avatar src to currentUser's img

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
         {currentUser ? (
           <User>
             <VideoCallIcon />
-            <Avatar />
+            <Avatar src={currentUser.img} />
             {currentUser.name}
           </User>
         ) : (
