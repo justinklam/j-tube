b2bf463ab866627d15c09abf37f440db424d8472
Justin Lam <justin.kh.lam@gmail.com>
2022-08-31 00:30:08 -0400

    add in dispatch forlike in handleLike function. import in like from videoSlice

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -4,7 +4,12 @@ import axios from "axios";
 import { format } from "timeago.js";
 // React Redux
 import { useDispatch, useSelector } from "react-redux";
-import { fetchFailure, fetchStart, fetchSuccess } from "../redux/videoSlice";
+import {
+  fetchFailure,
+  fetchStart,
+  fetchSuccess,
+  like,
+} from "../redux/videoSlice";
 // React Router Dom
 import { useLocation } from "react-router-dom";
 
@@ -154,6 +159,7 @@ const Video = () => {
 
   const handleLike = async () => {
     await axios.put(`/users/like/${currentVideo._id}`);
+    dispatch(like(currentUser._id));
   };
 
   const handleDislike = async () => {
