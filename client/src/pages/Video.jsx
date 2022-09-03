afc63e6981f0c797c3749cc995329c9d7d3b7b4d
Justin Lam <justin.kh.lam@gmail.com>
2022-09-03 12:36:36 -0400

    pass in videoId as props to Comments compoment

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -250,7 +250,7 @@ const Video = () => {
             </Subscribe>
           </Channel>
           <Hr />
-          <Comments />
+          <Comments videoId={currentVideo._id} />
         </Content>
       )}
       {/* <Recommendation>
