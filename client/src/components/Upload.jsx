fa5750f339afc9ce1c73921fc84e13cc0a3cba4a
Justin Lam <justin.kh.lam@gmail.com>
2022-09-08 15:47:45 -0400

    add styling to container component

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #000000a7;
+`;
 
 const Wrapper = styled.div``;
 
