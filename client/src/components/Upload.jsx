057f90b56cea8a56178c5f446a7265cc1183cff1
Justin Lam <justin.kh.lam@gmail.com>
2022-09-10 13:55:22 -0400

    add in base firebase uploadTask functionality

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -89,11 +89,31 @@ const Upload = ({ setOpen }) => {
     setTags(e.target.value.split(","));
   };
 
-  const uploadFile = (file) => {
+  const uploadFile = (file, urlType) => {
     const storage = getStorage();
     // Filename will use Date + file name to avoid conflicts
     const fileName = new Date().getTime + file.name;
-    const storageRef = ref(storage, file.name);
+    const storageRef = ref(storage, fileName);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {},
+      () => {}
+    );
   };
 
   // For Video
