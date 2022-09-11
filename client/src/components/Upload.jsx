e3a24286d8a30afa6536d142157f96f13cba6a9f
Justin Lam <justin.kh.lam@gmail.com>
2022-09-11 16:19:35 -0400

    switch percentage rendering to useState

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -107,7 +107,9 @@ const Upload = ({ setOpen }) => {
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
+        urlType === "imgUrl"
+          ? setImgPercentage(progress)
+          : setVidPercentage(progress);
         switch (snapshot.state) {
           case "paused":
             console.log("Upload is paused");
