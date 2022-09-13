b571d1f92e98b74b1fc6d1a2a2e85fcb4cfdc305
Justin Lam <justin.kh.lam@gmail.com>
2022-09-13 00:55:35 -0400

    import card component. loop over videos and load to Card. import in tags props

--- client/src/components/Recommendation.jsx
+++ client/src/components/Recommendation.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
+// Components
+import Card from "./Card";
+
 const Container = styled.div``;
 
-const Recommendation = () => {
+const Recommendation = ({ tags }) => {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
@@ -15,7 +18,13 @@ const Recommendation = () => {
     fetchVideos();
   }, [tags]);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card key={video._id} video={video} />
+      ))}
+    </Container>
+  );
 };
 
 export default Recommendation;
