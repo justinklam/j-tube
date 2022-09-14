be8a0bb7c1c879dcd2eea91eed5fd5b451a06608
Justin Lam <justin.kh.lam@gmail.com>
2022-09-14 01:11:53 -0400

    import in useLocation hook. add in axios query logic and setVideo state. add dependency array

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
+// React Router Dom
+import { useLocation } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
@@ -10,12 +12,15 @@ const Container = styled.div`
 
 const Search = () => {
   const [videos, setVideos] = useState([]);
+  const query = useLocation().search;
 
   useEffect(() => {
     const fetchVideos = async () => {
-      const res = await axios.get(`/videos/search`);
+      const res = await axios.get(`/videos/search${query}`);
+      setVideos(res.data);
     };
-  }, []);
+    fetchVideos();
+  }, [query]);
 
   return <Container />;
 };
