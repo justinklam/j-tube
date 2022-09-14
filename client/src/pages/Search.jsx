c7046d3a76ed03315f185acd6aa9e3eb2d97b5d4
Justin Lam <justin.kh.lam@gmail.com>
2022-09-14 01:13:56 -0400

    add in return render logic. map over videos in video state. import Card component and add in key

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 // React Router Dom
 import { useLocation } from "react-router-dom";
 
+// Components
+import Card from "../components/Card";
+
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -22,7 +25,13 @@ const Search = () => {
     fetchVideos();
   }, [query]);
 
-  return <Container />;
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card key={video._id} video={video} />
+      ))}
+    </Container>
+  );
 };
 
 export default Search;
