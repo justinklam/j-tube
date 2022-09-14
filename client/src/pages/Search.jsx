adc56547f3130f48d8b4c6d67e1eb256197ea79b
Justin Lam <justin.kh.lam@gmail.com>
2022-09-14 01:03:23 -0400

    add in base styling for container element. add in useState

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
 
 const Search = () => {
+  const [videos, setVideos] = useState();
+
   return <Container />;
 };
 
